53670c4ec6491fad7daf3c98e9c3ae49af5e84d1
Matvey Butolin <matvei31055@gmail.com>
2026-01-27 15:22:27 +0500

    refactor(tui): create tree init method

--- tui-app.py
+++ tui-app.py
@@ -2,34 +2,34 @@ from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, Tree
 
 from api_methods import get_electives, get_lessons
+from entities import Lesson
 from settings import settings
 
 
-class MyTree(Tree):
+class MenuTree(Tree):
+    def __init__(self, label):
+        super().__init__(label)
+        self.electives = self.root.add("Дисциплины")
+        for elective in get_electives(settings.menu_id):
+            elective_node = self.electives.add(label=elective.name, data=elective)
+            for lesson in elective.children:
+                elective_node.add(label=lesson.name, data=lesson)
+
     def on_tree_node_expanded(self, message: Tree.NodeExpanded) -> None:
         node = message.node
-        if node.data and node.data[0] == "lesson":
-            lessons = get_lessons(settings.menu_id, lesson_id=node.data[1])
+        if node.data and isinstance(node.data, Lesson):
+            lessons = get_lessons(settings.menu_id, lesson_id=node.data.id)
             for lesson in lessons:
-                lesson_node = node.add(label=lesson.name, data=("cycle", lesson.id))
+                lesson_node = node.add(label=lesson.name, data=lesson)
                 for team in lesson.teams:
-                    team_node = lesson_node.add(label=team.name, data=("team", team.id))
+                    team_node = lesson_node.add(label=team.name, data=team)
                     for professor in team.professors:
-                        team_node.add_leaf(label=professor.name, data=("professor"))
+                        team_node.add_leaf(label=professor.name, data=professor)
 
 
 class AmogusApp(App):
     def compose(self) -> ComposeResult:
-        tree: MyTree[str] = MyTree("Меню выбора")
-        electives = tree.root.add("Дисциплины")
-        for elective in get_electives(settings.menu_id):
-            elective_node = electives.add(
-                label=elective.name, data=("elective", elective.id)
-            )
-            for lesson in elective.children:
-                elective_node.add(label=lesson.name, data=("lesson", lesson.id))
-
-        yield tree
+        yield MenuTree("Меню выбора")
         yield Header()
         yield Footer()
 
