3ca540bcbcffba0741a306652bbbe2ef959e4c14
Matvey Butolin <matvei31055@gmail.com>
2026-03-31 20:28:43 +0500

    feat: better ux
    - Add DataTable to show the push results
    - Add visible timer

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.widgets import (
+    DataTable,
     Footer,
     Header,
+    Static,
     TabbedContent,
     TabPane,
 )
@@ -52,16 +54,23 @@ class AmogusApp(App):
 
     def compose(self) -> ComposeResult:
         with TabbedContent():
-            with TabPane("Tree", id="menu-tab"):
+            with TabPane("Меню выбора", id="menu-tab"):
                 yield MenuTree(
                     label="Меню выбора",
                     cycle_service=self.cycle_service,
                     module_service=self.module_service,
                     select_service=self.select_service,
                 )
+                yield Static("Таймер: --:--:--", id="timer")
+            with TabPane("Результаты записи", id="table-tab"):
+                yield DataTable(id="push_table")
 
         yield Header()
         yield Footer()
+
+    def on_mount(self):
+        self.push_table = self.query_one("#push_table", DataTable)
+        self.push_table.add_columns("Lesson ID", "Status", "Message")
 
     def action_save(self):
         try:
@@ -84,6 +93,7 @@ class AmogusApp(App):
         if self._pushing:
             self.notify("Запись уже выполняется", severity="warning")
             return
+        self.notify("Выполняю запись", severity="information")
         self._pushing = True
         try:
             selection = self.select_service.get_teams_for_api()
@@ -92,17 +102,16 @@ class AmogusApp(App):
             self.notify(str(e), severity="error")
         finally:
             self._pushing = False
+            self.notify("Запись завершена", severity="information")
 
     def _on_push_progress(self, result: PushResult, completed: int, total: int):
-        if result.error is not None:
-            self.notify(f"Ошибка: {result.error}", severity="error")
-        else:
-            self.notify(f"Прогресс: {completed}/{total}")
-        if completed == total:
-            self.notify("Запись завершена")
+        status = "✅ OK" if result.success else "❌ ERR"
+        message = result.error or result.text or ""
+        self.push_table.add_row(result.lesson_id, status, message)
 
     async def action_set_timer(self):
         """Установить таймер на время, указанное в settings.pick_time."""
+        # TODO Запретить запуск таймера, если нет выбранных предметов.
         if self._timer is None:
             self._timer = Timer(
                 pick_time=self.settings.pick_time, on_complete=self.action_push
@@ -111,5 +120,7 @@ class AmogusApp(App):
             self.notify(
                 f"Запуск таймера на {self.settings.pick_time}", severity="warning"
             )
+            timer_widget = self.query_one("#timer", Static)
+            timer_widget.update(f"Таймер: {self.settings.pick_time}")
         else:
             self.notify("Таймер уже запущен")
